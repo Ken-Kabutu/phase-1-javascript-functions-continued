5947447f5d5dd73a79a6cf7bc7f8500b4a390de4
Ken <kabutukenneth3@gmail.com>
2023-04-08 01:03:48 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+  return `This saturday, I want to ${activity}!`
+}
+console.log(saturdayFun());
+
+console.log(saturdayFun("bathe my dog"));
+
+
+const mondayWork = function(task = "go to the office."){
+  return `This Monday, I will ${task}`
+}
+console.log(mondayWork());
+
+console.log(mondayWork("I will work from home."));
+
+
+function wrapAdjective(parameter = "||"){
+  return const = function innerFun(flair = "special"){
+    return `You are ${parameter}, ${flair}, ${parameter}!`;
+  }
+  
+}
+console.log(innerFun("a dedicated programmer"));
+
+//console.log(wrapAdjective("%"));
+
+
+
